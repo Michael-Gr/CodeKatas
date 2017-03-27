895133cf46ebc0b9e38bcfa50070aa19402c299c
Michael Grein <michaelgrein92@gmail.com>
2017-03-27 15:07:13 -0400

    First kata solution added :)

---
+++ Multiply.rb
@@ -0,0 +1,19 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/multiply/train/ruby
+#
+################
+# Description: #
+################
+#
+# Fix the code to make the items multiply.
+#
+###########
+# Answer: #
+###########
+
+def multiply(a, b)
+  a * b
+end
