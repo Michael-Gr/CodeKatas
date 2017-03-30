be681faa999a849415d47c64d171b5dc004562f7
Michael Grein <michaelgrein92@gmail.com>
2017-03-30 15:40:21 -0400

    Remove string spaces

---
+++ Remove_string_spaces.rb
@@ -0,0 +1,19 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+#
+################
+# Description: #
+################
+#
+# Simple, remove the spaces from the string, then return the resultant string.
+#
+###########
+# Answer: #
+###########
+
+def no_space(x)
+  x.delete(" ")
+end
