15afa2bb43b1d70edcdbdbece5ba8969959749fb
Michael Grein <michaelgrein92@gmail.com>
2017-03-30 15:38:58 -0400

    Character from ASCII value

---
+++ Character_from_ASCII_value.rb
@@ -0,0 +1,27 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/55ad04714f0b468e8200001c
+#
+################
+# Description: #
+################
+#
+# Get character from ASCII Value
+#
+# Write a function getChar which takes a number and returns the corresponding ASCII char for that value.
+#
+# Example:
+#
+# getChar(65) # => 'A'
+# For ASCII table, you can refer to http://www.asciitable.com/
+#
+#
+###########
+# Answer: #
+###########
+
+def getChar(c)
+  c.chr
+end
