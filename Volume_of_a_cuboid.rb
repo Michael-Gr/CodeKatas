18a7e945a8850d52f3858ae03fac9df736f27a53
Michael Grein <michaelgrein92@gmail.com>
2017-04-03 14:23:25 -0400

    Volume of a cuboid

---
+++ Volume_of_a_cuboid.rb
@@ -0,0 +1,19 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/58261acb22be6e2ed800003a
+#
+################
+# Description: #
+################
+#
+# Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid. Write a function to help Bob with this calculation.
+#
+###########
+# Answer: #
+###########
+
+def get_volume_of_cuboid(length, width, height)
+  length * width * height
+end
