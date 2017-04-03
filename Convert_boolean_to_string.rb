0c9606bd7b67512f46577e6b2f4bbb935f83be5e
Michael Grein <michaelgrein92@gmail.com>
2017-04-03 14:24:57 -0400

    Convert a boolean to a string value

---
+++ Convert_boolean_to_string.rb
@@ -0,0 +1,21 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/53369039d7ab3ac506000467
+#
+################
+# Description: #
+################
+#
+# Complete the bool_to_word (Javascript: boolToWord ) method.
+#
+# Given: a boolean value
+#
+###########
+# Answer: #
+###########
+
+def bool_to_word(bool)
+  bool == true ? 'Yes' : 'No'
+end
