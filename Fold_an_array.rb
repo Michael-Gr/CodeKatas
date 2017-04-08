5739c4695e70d1908a2377c7c9c59540e4c08afa
Michael Grein <michaelgrein92@gmail.com>
2017-04-07 22:38:32 -0400

    Fold an array

---
+++ Fold_an_array.rb
@@ -0,0 +1,57 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/57ea70aa5500adfe8a000110
+#
+################
+# Description: #
+################
+#
+# Fold an array
+#
+# In this kata you have to write a method that folds a given array of integers by the middle x-times.
+#
+# An example says more than thousand words:
+#
+# Fold 1-times:
+# [1,2,3,4,5] -> [6,6,3]
+#
+# A little visualization (NOT for the algorithm but for the idea of folding):
+#
+#  Step 1         Step 2        Step 3       Step 4       Step5
+#                      5/           5|         5\
+#                     4/            4|          4\
+# 1 2 3 4 5      1 2 3/         1 2 3|       1 2 3\       6 6 3
+# ----*----      ----*          ----*        ----*        ----*
+#
+#
+# Fold 2-times:
+# [1,2,3,4,5] -> [9,6]
+# As you see, if the count of numbers is odd, the middle number will stay. Otherwise the fold-point is between the middle-numbers, so all numbers would be added in a way.
+#
+# The array will always contain numbers and will never be null. The parameter runs will always be a positive integer greater than 0 and says how many runs of folding your method has to do.
+#
+# If an array with one element is folded, it stays as the same array.
+#
+# The input array should not be modified!
+#
+###########
+# Answer: #
+###########
+
+def fold_array(array, runs)
+  answer = array
+  runs.times do
+    if answer.length == 1
+      answer
+    elsif answer.length == 2
+      answer = [answer.inject(:+)]
+    elsif answer.length.even? && answer.length != 2
+      answer = answer.map.with_index { |num, index| num + answer[(-1 * index) - 1] }[0...(answer.length/2)]
+    elsif answer.length.odd? && answer.length != 1
+      answer = answer[0...(answer.length/2).floor].map.with_index { |num, index| num + answer[(-1 * index) - 1] } << answer[(answer.length/2).ceil]
+    end
+  end
+  return answer
+end
