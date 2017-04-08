7e6488dfe3caa5f5e8870d94d619baf51872c491
Michael Grein <michaelgrein92@gmail.com>
2017-04-07 22:34:28 -0400

    Pre fizz buzz

---
+++ Pre_fizz_buzz.rb
@@ -0,0 +1,25 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/569e09850a8e371ab200000b
+#
+################
+# Description: #
+################
+#
+# This is the first step to understanding FizzBuzz.
+#
+# Your inputs: a positive integer, n, greater than or equal to one. n is provided, you have NO CONTROL over its value.
+#
+# Your expected outputs: an array of positive integers from 1 to n
+#
+# Your job is to write an algorithm that gets you from the input to the output.
+#
+###########
+# Answer: #
+###########
+
+def pre_fizz(n)
+  (1..n).to_a
+end
