cd07cb580fc503324f761486f0e3882067e5a83d
Michael Grein <michaelgrein92@gmail.com>
2017-04-10 23:05:50 -0400

    Stop spinning my words

---
+++ Stop_gninnipS_My_sdroW.rb
@@ -0,0 +1,19 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/5264d2b162488dc400000001
+#
+################
+# Description: #
+################
+#
+# Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+#
+###########
+# Answer: #
+###########
+
+def spinWords(string)
+  string.split(" ").map { |word| word.length >= 5 ? word.reverse : word }.join(" ")
+end
