f2c025f1d82dfaff3b139651440fc423882cef24
Michael Grein <michaelgrein92@gmail.com>
2017-04-11 13:58:32 -0400

    Even or odd

---
+++ Even_or_odd.rb
@@ -0,0 +1,19 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+#
+################
+# Description: #
+################
+#
+# Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+#
+###########
+# Answer: #
+###########
+
+def even_or_odd(number)
+  number.even? ? "Even" : "Odd"
+end
