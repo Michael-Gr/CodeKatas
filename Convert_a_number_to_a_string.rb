c2182af97d7afa1ede649c3ca774ff59f9619803
Michael Grein <michaelgrein92@gmail.com>
2017-04-13 22:11:09 -0400

    Convert a Number to a String

---
+++ Convert_a_number_to_a_string.rb
@@ -0,0 +1,26 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+#
+################
+# Description: #
+################
+#
+# We need a function that can transform a number into a string.
+#
+# What ways of achieving this do you know?
+#
+# Examples:
+#
+# numberToString(123); // returns '123';`
+# numberToString(999); // returns '999';`
+#
+###########
+# Answer: #
+###########
+
+def numberToString(num)
+  num.to_s
+end
