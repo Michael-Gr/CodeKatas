631c2bb68ab90a163d360ce2a82f6fa0aca7e93f
Michael Grein <michaelgrein92@gmail.com>
2017-04-13 22:12:44 -0400

    Opposite Number

---
+++ Opposite_number.rb
@@ -0,0 +1,26 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/56dec885c54a926dcd001095
+#
+################
+# Description: #
+################
+#
+# Very simple, given a number, find its opposite.
+#
+# Examples:
+#
+# 1: -1
+# 14: -14
+# -34: 34
+# But can you do it in 1 line of code and without any conditionals?
+#
+###########
+# Answer: #
+###########
+
+def opposite(x)
+ x * -1
+end
