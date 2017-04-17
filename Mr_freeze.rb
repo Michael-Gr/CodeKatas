1869ea5565bb8246991f439e6f8a9c9b93116e4a
Michael Grein <michaelgrein92@gmail.com>
2017-04-17 17:29:22 -0400

    Mr. Freeze

--- Mr_freeze.rb
+++ Mr_freeze.rb
@@ -2,14 +2,16 @@
 # Link to kata: #
 #################
 #
-#
+# https://www.codewars.com/kata/514a3996d22ce03198000003
 #
 ################
 # Description: #
 ################
 #
-#
+# There is an object/class already created called MrFreeze. Mark this object as frozen so that no other changes can be made to it.
 #
 ###########
 # Answer: #
 ###########
+
+MrFreeze.freeze
