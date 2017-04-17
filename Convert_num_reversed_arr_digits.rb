2f5640af21b0241cecb1f19bdd8f9f9392feb892
Michael Grein <michaelgrein92@gmail.com>
2017-04-17 17:31:11 -0400

    Convert a number into a reversed array of digits

--- Convert_num_reversed_arr_digits.rb
+++ Convert_num_reversed_arr_digits.rb
@@ -2,14 +2,18 @@
 # Link to kata: #
 #################
 #
-#
+# https://www.codewars.com/kata/5583090cbe83f4fd8c000051
 #
 ################
 # Description: #
 ################
 #
-#
+# Given a random number, You have to return the digits of this number within an array in reverse order.
 #
 ###########
 # Answer: #
 ###########
+
+def digitize(n)
+  n.to_s.reverse.split("").map(&:to_i)
+end
