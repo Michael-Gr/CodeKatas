40b3540ec6a832d29fdd90c02d172301e840b623
Michael Grein <michaelgrein92@gmail.com>
2017-04-17 17:26:56 -0400

    return negative

---
+++ Return_negative.rb
@@ -0,0 +1,19 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/55685cd7ad70877c23000102
+#
+################
+# Description: #
+################
+#
+# In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+#
+###########
+# Answer: #
+###########
+
+def makeNegative(num)
+  num <= 0 ? num : -num
+end
