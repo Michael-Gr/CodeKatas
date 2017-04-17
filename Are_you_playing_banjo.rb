34470449b0aab7a9eea30aec3c46b93a71474120
Michael Grein <michaelgrein92@gmail.com>
2017-04-17 17:28:18 -0400

    Are you playing banjo

--- Are_you_playing_banjo.rb
+++ Are_you_playing_banjo.rb
@@ -2,14 +2,22 @@
 # Link to kata: #
 #################
 #
-#
+# https://www.codewars.com/kata/53af2b8861023f1d88000832
 #
 ################
 # Description: #
 ################
 #
+# Create a function which answers the question "Are you playing Banjo?". If your name starts with the letter "R" or lower case "r", you are playing Banjo!
 #
+# The function takes a name as its only argument, and returns one of the following strings:
+#
+# C# name + "plays banjo" name + "does not play banjo"
 #
 ###########
 # Answer: #
 ###########
+
+def are_you_playing_banjo(name)
+    name.downcase[0] == "r" ? name + " plays banjo" : name + " does not play banjo"
+end
