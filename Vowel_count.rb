f90801b4773fb0afa135d7240cdf6aeb84ffdcac
Michael Grein <michaelgrein92@gmail.com>
2017-04-19 00:09:05 -0400

    Vowel count

---
+++ Vowel_count.rb
@@ -0,0 +1,21 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/54ff3102c1bad923760001f3
+#
+################
+# Description: #
+################
+#
+# Return the number (count) of vowels in the given string.
+#
+# We will consider a, e, i, o, and u as vowels for this Kata.
+#
+###########
+# Answer: #
+###########
+
+def getCount(str)
+  str.count("AEIOUaeiou")
+end
