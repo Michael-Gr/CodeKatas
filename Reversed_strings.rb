a21dfdf0b77fd630b86a8144b5e564c23b2dcb13
Michael Grein <michaelgrein92@gmail.com>
2017-05-01 00:22:34 -0400

    Reversed Strings

---
+++ Reversed_strings.rb
@@ -0,0 +1,21 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/5168bb5dfe9a00b126000018
+#
+################
+# Description: #
+################
+#
+# Complete the solution so that it reverses the string value passed into it.
+#
+# solution('world') # returns 'dlrow'
+#
+###########
+# Answer: #
+###########
+
+def solution(str)
+  str.reverse
+end
