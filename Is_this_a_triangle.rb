4825f3317ae33f3c69dc80122499e0ff98bd410f
Michael Grein <michaelgrein92@gmail.com>
2017-04-30 23:55:12 -0400

    Is this a triangle?

---
+++ Is_this_a_triangle.rb
@@ -0,0 +1,21 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/56606694ec01347ce800001b
+#
+################
+# Description: #
+################
+#
+# Implement a method that accepts 3 integer values a, b, c. The method should return true if a triangle can be built with the sides of given length and false in any other case.
+#
+# (In this case, all triangles must have surface greater than 0 to be accepted).
+#
+###########
+# Answer: #
+###########
+
+def isTriangle(a,b,c)
+   a + b > c && b + c > a && a + c > b
+end
