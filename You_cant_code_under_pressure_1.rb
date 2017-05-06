187d193a09dd008b2b96985d07ee6a9a59304c1c
Michael Grein <michaelgrein92@gmail.com>
2017-05-05 23:18:02 -0400

    You cant code under pressure

---
+++ You_cant_code_under_pressure_1.rb
@@ -0,0 +1,19 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/53ee5429ba190077850011d4
+#
+################
+# Description: #
+################
+#
+# Code as fast as you can! You need to double the integer and return it.
+#
+###########
+# Answer: #
+###########
+
+def doubleInteger(i)
+  i*2
+end
