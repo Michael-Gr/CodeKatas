a7a726148f2c9d896fb0bbb6859d2dd4d8886fcc
Michael Grein <michaelgrein92@gmail.com>
2017-05-10 00:06:58 -0400

    Multiplying two numbers

---
+++ Multiplying_two_numbers.rb
@@ -0,0 +1,19 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/523b66342d0c301ae400003b
+#
+################
+# Description: #
+################
+#
+# This function has to be called multiply and needs to take two numbers as arguments, and has to return the multiplication of the two arguments.
+#
+###########
+# Answer: #
+###########
+
+def multiply a,b
+  a*b
+end
