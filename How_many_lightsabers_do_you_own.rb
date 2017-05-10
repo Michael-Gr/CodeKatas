c1cb5afca21a37eba453fffb22bbe03c0327ba5a
Michael Grein <michaelgrein92@gmail.com>
2017-05-09 23:59:28 -0400

    How many lightsabers do you own?

---
+++ How_many_lightsabers_do_you_own.rb
@@ -0,0 +1,20 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/51f9d93b4095e0a7200001b8
+#
+################
+# Description: #
+################
+#
+# Loading description...
+#
+###########
+# Answer: #
+###########
+
+def howManyLightsabersDoYouOwn(*name)
+  p name
+  name.first == "Zach" ? 18 : 0
+end
