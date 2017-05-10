ef4b659e032c4a653db7a0c7649c4e94321268a5
Michael Grein <michaelgrein92@gmail.com>
2017-05-09 23:52:27 -0400

    Sort Arrays

---
+++ Sort_arrays.rb
@@ -0,0 +1,19 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/51f41b98e8f176e70d0002a8
+#
+################
+# Description: #
+################
+#
+# Just a simple sorting usage. Create a function that returns the elements of the input-array in a sorted manner.
+#
+###########
+# Answer: #
+###########
+
+def sortme( names )
+  names.sort
+end
