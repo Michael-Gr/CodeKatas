d2a651af01511b45390156c67ab1d48c78ea3842
Michael Grein <michaelgrein92@gmail.com>
2017-05-12 18:11:45 -0400

    Incorrect division method

---
+++ Incorrect_division_method.rb
@@ -0,0 +1,19 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/54d1c59aba326343c80000e7
+#
+################
+# Description: #
+################
+#
+# This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+#
+###########
+# Answer: #
+###########
+
+def divide_numbers(x, y)
+  x.to_f / y.to_f
+end
