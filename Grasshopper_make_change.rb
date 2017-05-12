93ef52c8a9986cab70acd0a400b588d721223327
Michael Grein <michaelgrein92@gmail.com>
2017-05-11 23:36:03 -0400

    Grasshopper - Make Change

---
+++ Grasshopper_make_change.rb
@@ -0,0 +1,35 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/560dab9f8b50f89fd6000070
+#
+################
+# Description: #
+################
+#
+# Get change
+#
+# You go to the store and have a 10 dollar bill to spend. You buy candy, chips, and soda. Find out how much change you get back from the cashier.
+#
+# Item	Cost
+# Candy	$1.42
+# Chips	$2.40
+# Soda	$1.00
+# Create 5 variables and use the cost from the table above to set their values.
+#
+# money
+# candy
+# chips
+# soda
+# change
+#
+###########
+# Answer: #
+###########
+
+money = 10.00
+candy = 1.42
+chips = 2.40
+soda = 1.00
+change = money - candy - chips - soda
