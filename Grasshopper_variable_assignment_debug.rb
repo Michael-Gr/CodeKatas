0344d1344f28e532dca9c0474ff237cce5747b13
Michael Grein <michaelgrein92@gmail.com>
2017-05-11 23:40:33 -0400

    Grasshopper Variable Assignment

---
+++ Grasshopper_variable_assignment_debug.rb
@@ -0,0 +1,20 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/5612e743cab69fec6d000077
+#
+################
+# Description: #
+################
+#
+# Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+#
+###########
+# Answer: #
+###########
+
+a = "dev"
+b = "Lab"
+
+name = a + b
