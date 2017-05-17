6229f79579f85189e57a468de6810ce2c27d28f1
Michael Grein <michaelgrein92@gmail.com>
2017-05-16 22:27:16 -0400

    String repeat.

---
+++ String_repeat.rb
@@ -0,0 +1,22 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
+#
+################
+# Description: #
+################
+#
+# Write a function called repeatStr which repeats the given string string exactly n times.
+#
+# repeatStr(6, "I") // "IIIIII"
+# repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+#
+###########
+# Answer: #
+###########
+
+def repeat_str (n, s)
+  s * n
+end
