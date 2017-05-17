c2c04995f5668e6e41dbd2cdd73be4d64a70ffb0
Michael Grein <michaelgrein92@gmail.com>
2017-05-16 22:38:18 -0400

    Basic training: Adding an item to an array

---
+++ Basic_training_add_item_to_an_array.rb
@@ -0,0 +1,22 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/511f0fe64ae8683297000001
+#
+################
+# Description: #
+################
+#
+# Add the value "codewars" to the websites array.
+# After your code executes the websites array should == ["codewars"]
+#
+# The websites array has already been defined for you using the following code:
+#
+# websites = []
+#
+###########
+# Answer: #
+###########
+
+websites << "codewars"
