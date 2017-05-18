5e065517d16972c82a1a16d7e8cf110f90cbf4d4
Michael Grein <michaelgrein92@gmail.com>
2017-05-17 22:46:34 -0400

    Function two - Squaring an argument

---
+++ Function_two_squaring_an_argument.rb
@@ -0,0 +1,19 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/523b623152af8a30c6000027
+#
+################
+# Description: #
+################
+#
+# Now you have to write a function called square that takes an argument and returns the square of it.
+#
+###########
+# Answer: #
+###########
+
+def square(n)
+  n ** 2
+end
