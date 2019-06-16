f4587eeb37c8b5b08f33181702d4e4aec1574072
Michael Grein <michaelgrein92@gmail.com>
2019-06-16 16:06:13 -0400

    Number to String - Ruby

---
+++ Number_to_string.rb
@@ -0,0 +1,21 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/53934feec44762736c00044b
+#
+################
+# Description: #
+################
+#
+# The code gives an error!
+#
+# a = 123.toString
+#
+# Fix it!
+#
+###########
+# Answer: #
+###########
+
+a = some_number.to_s
