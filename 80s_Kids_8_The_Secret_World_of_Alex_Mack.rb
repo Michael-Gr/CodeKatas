d120298a5a229eeca35f0f2ca664e7973cc31217
Michael Grein <michaelgrein92@gmail.com>
2019-07-01 17:44:17 -0400

    80's Kids #8: The Secret World of Alex Mack - Ruby

---
+++ 80s_Kids_8_The_Secret_World_of_Alex_Mack.rb
@@ -0,0 +1,44 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/56748c679c14c10b6e000022
+#
+################
+# Description: #
+################
+#
+# If you were born in the 80's you were probably a pre-teen when the Secret World of Alex Mack first aired. I'll be honest, the young superhero was my first crush. Typical superhero story: get slimed by radioactive gunk, obtain super powers, struggle to learn more about your powers while uncovering the truth about yourself, and most importantly, keep your identity a secret.
+#
+# Top executives at Paradise Valley Chemical Plant are close to finding the superhero's true identity. They know that someone who works at the plant is related to her (Shhh, it's her dad!). Danielle Atron has hired you to obtain the badge number of this person.
+#
+# Can you find the Secret badge number of Alex Mack's dad?
+#
+# Note: This is a puzzle. The initial solution is intentionally vague. Good luck!
+#
+# Check out my other 80's Kids Katas:
+# 80's Kids #1: How Many Licks Does It Take
+# 80's Kids #2: Help Alf Find His Spaceship
+#
+# 80's Kids #3: Punky Brewster's Socks
+#
+# 80's Kids #4: Legends of the Hidden Temple
+#
+# 80's Kids #5: You Can't Do That on Television
+#
+# 80's Kids #6: Rock 'Em, Sock 'Em Robots
+#
+# 80's Kids #7: She's a Small Wonder
+#
+# 80's Kids #8: The Secret World of Alex Mack
+#
+# 80's Kids #9: Down in Fraggle Rock 
+#
+# 80's Kids #10: Captain Planet 
+# 
+###########
+# Answer: #
+###########
+
+p secret
+badge = Time.now.to_i * 0.333
