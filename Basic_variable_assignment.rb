b84fdaeac399a35f7ce1a974c6efa4fd8167ea19
Michael Grein <michaelgrein92@gmail.com>
2019-08-13 10:53:28 -0400

    Basic variable assignment - Ruby

---
+++ Basic_variable_assignment.rb
@@ -0,0 +1,19 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/50ee6b0bdeab583673000025
+#
+################
+# Description: #
+################
+#
+# This code should store "codewa.rs" as a variable called "name" but it's not working. Can you figure out why?
+#
+###########
+# Answer: #
+###########
+
+a = "code"
+b = "wa.rs"
+name = a + b
