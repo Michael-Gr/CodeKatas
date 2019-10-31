9aee66bb1ec00995342613ad16bbde944f2633fb
Michael Grein <michaelgrein92@gmail.com>
2019-10-30 20:36:14 -0400

    Remove First and Last Character - Ruby

---
+++ Remove_First_and_Last_Character.rb
@@ -0,0 +1,22 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0
+#
+################
+# Description: #
+################
+#
+# It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+#
+###########
+# Answer: #
+###########
+
+def remove_char(s)
+  s = s.chars
+  s.shift
+  s.pop
+  s.join
+end
