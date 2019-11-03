ab016e3a49d93ad4ef54fdb5a8e1c81fbc770a0a
Michael Grein <michaelgrein92@gmail.com>
2019-11-03 09:54:52 -0500

    Invert Values - Ruby

---
+++ Invert_values.rb
@@ -0,0 +1,27 @@
+#################
+# Link to kata: #
+#################
+#
+# https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad
+#
+################
+# Description: #
+################
+#
+# Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+#
+# invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+# invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+# invert([]) == []
+#
+# You can assume that all values are integers. Do not mutate the input array/list.
+#
+###########
+# Answer: #
+###########
+
+def invert(list)
+  list.map do |n|
+    n < 0 ? n.to_s.gsub('-', '').to_i : ("-" + n.to_s).to_i
+  end
+end
