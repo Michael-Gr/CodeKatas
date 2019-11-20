f745322fcfba5b68a637a3ac6f9878db00d989a4
Michael Grein <michaelgrein92@gmail.com>
2019-11-19 19:34:07 -0500

    Opposite number - C#

---
+++ Opposite_number.cs
@@ -0,0 +1,25 @@
+///////////////////
+// Link to kata: // 
+///////////////////
+//
+// https://www.codewars.com/kata/56dec885c54a926dcd001095
+//
+//////////////////
+// Description: //
+//////////////////
+//
+// Very simple, given a number, find its opposite.
+//
+/////////////
+// Answer: //
+/////////////
+
+using System;
+
+public class Kata
+{
+    public  static int Opposite(int number)
+    {
+        return number * -1;
+    }
+}
